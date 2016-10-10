c3793627d93d3569e10d21d9f569afcdf16809f5
slebit <tillnagotannat@gmail.com>
2016-10-10 12:18:35 +0200

    Tesla

--- tesla.mk
+++ tesla.mk
@@ -5,11 +5,11 @@ PRODUCT_RELEASE_NAME := flounder
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1536
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common Tesla stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 DEVICE_PACKAGE_OVERLAYS += device/htc/flounder/overlay-slim
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := slim_flounder
+PRODUCT_NAME := tesla_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
 
